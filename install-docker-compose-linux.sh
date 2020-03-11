3dc05aee4599fa3000a2af40ebc3b1eb7a20238c
Thanh Hoàng Lê Hải <ghostbb10@gmail.com>
2020-03-11 19:28:00 +0700

    add symbolic link

--- install-docker-compose-linux.sh
+++ install-docker-compose-linux.sh
@@ -2,11 +2,14 @@
 
 echo "Docker Commands Toolkit - by Thanh Hoang Le Hai @ghostbbbmt"
 
-echo "Installing Docker Compose"
+echo "Installing Docker Compose (Linux)"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 
 sudo chmod +x /usr/local/bin/docker-compose;
+
+# Create symbolic link to docker-compose binary (to use with sudo)
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 docker-compose --version;
 
