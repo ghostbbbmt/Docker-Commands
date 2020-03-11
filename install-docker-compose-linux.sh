b10c0dfb89b3150ad7ebe13e6bdb9db989ce9a79
Thanh Hoàng Lê Hải <ghostbb10@gmail.com>
2020-03-11 18:04:39 +0700

    change to latest

--- install-docker-compose-linux.sh
+++ install-docker-compose-linux.sh
@@ -4,7 +4,7 @@ echo "Docker Commands Toolkit - by Thanh Hoang Le Hai @ghostbbbmt"
 
 echo "Installing Docker Compose"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 
 sudo chmod +x /usr/local/bin/docker-compose;
 
